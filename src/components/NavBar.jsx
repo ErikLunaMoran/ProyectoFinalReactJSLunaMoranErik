e3759f9be71178a5df2d9c4a914fe3cb3987bc9e
Erik Luna Morán <eriklunamoran@gmail.com>
2023-07-06 15:18:10 -0300

    corrección brand

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 import { CartWidget } from "./CartWidget";
 
@@ -15,7 +15,9 @@ console.log([...unique]);
 export const NavBar = () => (
   <Navbar bg="dark" variant="dark">
     <Container>
-      <Navbar.Brand>Mi Castillo</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">
+        Cat Food
+      </Navbar.Brand>
       <Nav className="me-auto">
         {[...unique].map((item) => (
           <NavLink key={item} className="nav-link" to={`/category/${item}`}>
